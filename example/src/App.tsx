51c2b78c99da71fac83d0b2124497bb05dafb4d2
Karthik K <karthiknetworklink@gmail.com>
2020-06-28 22:00:14 +0530

    fix: style props

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
         <div>
             Do something
         </div>
-        <Flex w={'60%'} justifyContent='center' alignItems={'center'} alignContent={'center'} p={2} style={{
+        <Flex w={'100%'} justifyContent='center' alignItems={'center'} alignContent={'center'} p={2} style={{
             backgroundColor: 'red'
         }}>
             <div>Div 1</div>
--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components'
 // eslint-disable-next-line no-unused-vars
 import { BlockBoxProps, getBlockBoxProps } from '../../mixins/box'
+// eslint-disable-next-line no-unused-vars
+import { SizeProps, getSizeProps } from '../../mixins/size'
 
 export interface BoxProps {
   color: string
 }
 
-export const Box = styled.div<BoxProps & BlockBoxProps>`
+export const Box = styled.div<BoxProps & BlockBoxProps & SizeProps>`
   ${getBlockBoxProps}
+
+  ${getSizeProps}
 `
