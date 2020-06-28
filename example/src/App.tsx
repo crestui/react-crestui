0b454abd0b313d439336718a2247a5d8fd2de357
Karthik K <karthiknetworklink@gmail.com>
2020-06-28 23:11:39 +0530

    fix: inline

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -18,6 +18,34 @@ const App = () => {
                 <div>Div 1</div>
                 <div>Div 2</div>
             </Flex>
+            <Flex w={1} justifyContent='center' alignItems={'center'} alignContent={'center'}>
+                <Flex style={{
+                        backgroundColor: 'green'
+                        }}>
+                    <div>Green D1</div>
+                    <div>Green D2</div>
+                </Flex>
+                <Flex flexGrow={1} style={{
+                        backgroundColor: 'purple'
+                        }}>
+                    <div>Purple Div 3</div>
+                    <div>Purple Div 4</div>
+                </Flex>
+            </Flex>
+            <Flex w={1} justifyContent='center' alignItems={'center'} alignContent={'center'}>
+                <Flex flexGrow={1}  style={{
+                        backgroundColor: 'green'
+                        }}>
+                    <div>Green D1</div>
+                    <div>Green D2</div>
+                </Flex>
+                <Flex style={{
+                        backgroundColor: 'purple'
+                        }}>
+                    <div>Purple D3</div>
+                    <div>Purple D4</div>
+                </Flex>
+            </Flex>
             <Flex flexDirection='column' justifyContent='center' h={'50vh'}>
                 <Box py={1}>Div 1</Box>
                 <Box py={1}>Div 2</Box>
