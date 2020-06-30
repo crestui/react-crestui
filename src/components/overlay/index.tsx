d4b337cce80b87c81e2d38aca0e0e0b88400c7be
Karthik K <karthiknetworklink@gmail.com>
2020-06-30 12:48:02 +0530

    fix: various positions

--- src/components/overlay/index.tsx
+++ src/components/overlay/index.tsx
@@ -15,12 +15,77 @@ export interface OverlayProps {
 }
 
 function getCoords(node: Element, placement?: string): Coords {
-  console.info(placement)
-  const rect = node.getBoundingClientRect()
-  return {
-    left: rect.x + rect.width / 2,
-    top: rect.y + window.scrollY
+  if (placement === undefined || placement === null) {
+    placement = 'top-left'
   }
+  const rect = node.getBoundingClientRect()
+  switch (placement) {
+    case 'top-left':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'top-middle':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'top-right':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'left-top':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'left-middle':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'left-bottom':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'bottom-left':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'bottom-middle':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'bottom-right':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'right-top':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'right-middle':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    case 'right-bottom':
+      return {
+        left: rect.x + rect.width / 2,
+        top: rect.y + window.scrollY
+      }
+    default:
+      throw new Error(
+        `Placement for overlay ${placement} invalid. Not Supported`
+      )
+  }
+  console.info(placement)
 }
 
 /**
