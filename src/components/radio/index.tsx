4ff974358605adc40cf8123263ca45f5dc5cc8b8
Karthik K <karthiknetworklink@gmail.com>
2020-07-02 05:00:29 +0530

    fix: radiogroup and radio

--- src/components/radio/index.tsx
+++ src/components/radio/index.tsx
@@ -1,9 +1,37 @@
+import React, { createContext, useState, useContext } from 'react'
+
+interface RadioGroupState {
+  value: string
+
+  setValue: Function
+}
+
+const RadioGroupContext = createContext<RadioGroupState>({
+  value: '',
+  setValue: () => {}
+})
+
+export interface RadioGroupProps {
+  children: React.ReactNode
+}
+
+export const RadioGroup = (props: RadioGroupProps) => {
+  const [value, setValue] = useState('')
+  return (
+    <RadioGroupContext.Provider value={{ value, setValue }}>
+      {props.children}
+    </RadioGroupContext.Provider>
+  )
+}
+
 export interface RadioProps {
   label: string
 }
 
 export const Radio = (props: RadioProps) => {
+  const ctx = useContext(RadioGroupContext)
   console.info(props)
+  console.info(ctx)
   return null
 }
 
