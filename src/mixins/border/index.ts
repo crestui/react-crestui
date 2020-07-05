d2679fd7cbb8c382275dacd2c7ab545b940777ad
Karthik K <karthiknetworklink@gmail.com>
2020-07-05 15:29:41 +0530

    fix: add more details about box-shadows

--- src/mixins/border/index.ts
+++ src/mixins/border/index.ts
@@ -22,6 +22,13 @@ export interface BorderProps {
    * Radius indicates borderRadius and the units are in 'rem'
    */
   radius?: string | number
+
+  /**
+   * "The box-shadow CSS property adds shadow effects around an element's frame.
+   * You can set multiple effects separated by commas. "
+   * Courtesy:  https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow
+   */
+  shadow?: string
 }
 
 export const applyBorderProps = css<BorderProps>`
@@ -33,4 +40,6 @@ export const applyBorderProps = css<BorderProps>`
   ${({ bw }) => bw && 'border-width: ' + getSize(bw) + ';'}
 
   ${({ bc }) => bc && 'border-color: ' + getColor(bc) + ';'}
+
+  ${({ shadow }) => shadow && 'box-shadow: ' + shadow + ';'}
 `
