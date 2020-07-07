477a56f503fdabbe63b9b9ed95667caca850a7b5
Karthik K <karthiknetworklink@gmail.com>
2020-07-07 08:14:05 +0530

    fix: add gatsby config

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `React Components for CrestUI`,
+    description: `Description for react components for crest-ui library.`,
+    author: `@crestui`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
