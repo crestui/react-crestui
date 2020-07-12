0d49f02d5c802350016c40f5e8a85546f389124a
Karthik K <karthiknetworklink@gmail.com>
2020-07-12 13:04:33 +0530

    fix: tabs initial version

--- src/components/tabs/tabitem.tsx
+++ src/components/tabs/tabitem.tsx
@@ -4,16 +4,38 @@ import nextId from 'react-id-generator'
 import { TabContext } from './use-tab'
 import { displayNone } from '../../utils/display-none'
 
+const TabHeader = styled.div<{ selected: boolean }>`
+  background-color: transparent;
+  flex: 1 auto;
+  border-bottom: ${(props) => (props.selected ? '2px solid' : 'none')};
+  border-color: ${(props) => props.theme.colors.primary};
+  cursor: pointer;
+  min-height: 5vh;
+  height: 5vh;
+  line-height: 5vh;
+  text-align: center;
+  min-width: 15%;
+  padding: 0rem 0.5rem;
+`
+
 const TabContent = styled.div<{ selected: boolean }>`
   opacity: ${(props) => (props.selected ? 1 : 0)};
   transition: opacity 0.3s ease-in;
   ${(props) => (props.selected ? '' : displayNone())}
 `
 
+const TabHeaderSpan = styled.span`
+  display: inline-block;
+  vertical-align: middle;
+  line-height: normal;
+`
+
 export interface TabItemProps {
   title: string
 
   value: string
+
+  headingOnly: boolean
 
   children: React.ReactNode
 }
