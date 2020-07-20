f012173fc1c7fbfac1804641a192f84f08ceec00
Karthik K <karthiknetworklink@gmail.com>
2020-07-20 21:09:28 +0530

    fix: select

--- docs/src/pages/react-crestui/components/select/index.js
+++ docs/src/pages/react-crestui/components/select/index.js
@@ -1,22 +1,16 @@
 import React from "react"
 import ReactComponentsLayout from "@components/react-component-layout"
 import LiveCodeComponent from "@components/live-code"
-import { MQProvider, Dropdown, Box, onAlignElementsBottom } from 'react-crestui'
+import { MQProvider, Select, onAlignElementsBottom } from "react-crestui"
 
 const ReactCrestComponentSelect = () =>  {
   const code = `
   <MQProvider>
-    <Dropdown event='focus' label='Products' size={'medium'}
-        onAlignElements={onAlignElementsBottom('left')}>
-        <Box bg='transparent' py={1}>
-          <Box bg='white' radius={0.5} bs={'solid'} bw={0.25} bc={'primary'} p={3}>
-              Some interesting content here
-          </Box>
-        </Box>
-    </Dropdown>
+    <Select>
+    </Select>
   </MQProvider>
   `
-  const scope = { Dropdown, Box, MQProvider, onAlignElementsBottom }
+  const scope = { Select, MQProvider, onAlignElementsBottom }
   return (
     <ReactComponentsLayout title="Select - Components - React CrestUI">
       <h2>Select - Components - React CrestUI</h2>
