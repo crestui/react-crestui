b0d874e6efc88a5e4d6ca474ccab6777f52f2404
Karthik K <karthiknetworklink@gmail.com>
2020-07-21 20:04:52 +0530

    feat: add link

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -1,14 +1,2 @@
-import styled from 'styled-components'
-import { SizeProps, applySizeProps } from '../../mixins/size'
-import { ColorProps, applyColorProps } from '../../mixins/color'
-import { BorderProps, applyBorderProps } from '../../mixins/border'
-
-export const Link = styled.a<SizeProps & ColorProps & BorderProps>`
-  text-decoration: none;
-
-  ${applySizeProps}
-
-  ${applyColorProps}
-
-  ${applyBorderProps}
-`
+export * from './plain-link'
+export * from './nav-link'
---
+++ src/components/link/plain-link.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const PlainLink = styled.a`
+  text-decoration: none;
+  box-shadow: none;
+`
+
+export default PlainLink
