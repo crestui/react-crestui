d6d4807244a3f17e5900847576bbb1b21a8862c5
Karthik K <karthiknetworklink@gmail.com>
2020-07-21 14:31:30 +0530

    fix: add select

--- docs/src/pages/react-crestui/components/select/index.js
+++ docs/src/pages/react-crestui/components/select/index.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import ReactComponentsLayout from '@components/react-component-layout'
 import LiveCodeComponent from '@components/live-code'
-import { MQProvider, Select, onAlignElementsBottom } from 'react-crestui'
+import { MQProvider, Select, useSelect, SelectItem } from 'react-crestui'
 
 const ReactCrestComponentSelect = () => {
+  const items = new Array<SelectItem>({
+      label: 'Apple',
+      value: 'apple'
+    },
+    {
+      label: 'Orange',
+      value: 'orange'
+    },
+    {
+      label: 'Banana',
+      value: 'banana'
+    })
+  const selectState = useSelect(items, 'apple')
   const code = `
   <MQProvider>
-    <Select>
+    <Select selectState={selectState}>
     </Select>
   </MQProvider>
   `
-  const scope = { Select, MQProvider, onAlignElementsBottom }
+  const scope = { Select, MQProvider, selectState }
   return (
     <ReactComponentsLayout title="Select - Components - React CrestUI">
       <h2>Select - Components - React CrestUI</h2>
